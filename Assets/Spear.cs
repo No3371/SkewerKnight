573435f8a5812fb266d98c3b567f8208720c0f46
鍾孟儒 <t7214948ttt@gmail.com>
2016-08-24 20:56:04 +0800

    123mothafucka

---
+++ Assets/Spear.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spear : MonoBehaviour {
+	public bool attacking = false;
+	public float angle;
+	public GameObject character;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.X) && !attacking) {
+			attacking = true;
+		}
+
+		if(attacking) {
+			attack();
+		}
+
+		//public float slope = mathf.atan2()
+		character = GameObject.FindWithTag("character");
+		transform.position = new Vector2(character.transform.position.x - 0.43f,character.transform.position.y + 0.72f);
+	}
+
+	void FixedUpdate() {
+		//angle = Mathf.Atan2(500 - transform.position.y, 500 - transform.position.x) * Mathf.Rad2Deg;
+        //Debug.Log(mousePosition.x + " " + mousePosition.y);
+        //if(angle > this.transform.rotation.z) transform.Rotate(0, 0, -1);
+        //else if(angle < this.transform.rotation.z) transform.Rotate(0, 0, 1);
+	}
+
+	void attack() {
+
+	}
+}

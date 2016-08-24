a4f9d123c831c5bd944e679e456fe7635d656bba
m9731526 <laugholouder@gmail.com>
2016-08-24 22:49:56 +0800

    Action: Create Asset

---
+++ Assets/Sprite/Resources/CreateAsset.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class CreateAsset : MonoBehaviour {
+
+    [MenuItem("Assets/Create/AchievementDataBase")]
+    public static void Create()
+    {
+        AchievementsData asset = ScriptableObject.CreateInstance<AchievementsData>();
+        AssetDatabase.CreateAsset(asset, "Assets/AchievementData.asset");
+        AssetDatabase.SaveAssets();
+
+        EditorUtility.FocusProjectWindow();
+        Selection.activeObject = asset;
+    }
+}

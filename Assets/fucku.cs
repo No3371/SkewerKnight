25cc8ba5338994fe266a79d7dc9c12945bf3f654
鍾孟儒 <t7214948ttt@gmail.com>
2016-08-24 20:16:11 +0800

    first

---
+++ Assets/fucku.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fucku : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Input.mousePosition;
+
+	}
+}

fa15dc1bdb0e6c1f2afc0743a1d226cac54d1446
m9731526 <laugholouder@gmail.com>
2016-08-24 19:44:34 +0800

    GameManager

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager Instance;
+
+    public GameObject MainCamera;
+
+    public int Score;
+    float ScrollSpeed;
+    float Difficulty;
+
+    float GameStartTime;
+
+
+	// Use this for initialization
+	void Start () {
+        if (Instance == null) Instance = this;
+        if (Instance != this) Destroy(this);
+        DontDestroyOnLoad(this);
+
+        GameStartTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

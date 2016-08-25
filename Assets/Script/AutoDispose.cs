44482dcb91fd12663c6aec2715f8cb3ad29c9e2d
m9731526 <laugholouder@gmail.com>
2016-08-25 15:33:32 +0800

    Auto dispose objects out of screen

--- Assets/Script/AutoDispose.cs
+++ Assets/Script/AutoDispose.cs
@@ -7,9 +7,18 @@ public class AutoDispose : MonoBehaviour {
 	void Start () {
 	
 	}
+
+    void onDestroy()
+    {
+        if(this.gameObject.layer == 9)
+        {
+            MobManager.Instance.MobCount -= 1;
+        }
+        Destroy(this.gameObject);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (this.transform.position.x - GameManager.Instance.MainCamera.transform.position.x < -10) Destroy(this.gameObject);
+        if (this.transform.position.x - GameManager.Instance.MainCamera.transform.position.x < -15) Destroy(this);
 	}
 }

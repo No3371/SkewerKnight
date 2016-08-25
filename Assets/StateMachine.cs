87943aa792e4302d8338d56edec81d3b0c2a8a45
m9731526 <laugholouder@gmail.com>
2016-08-25 11:02:35 +0800

    StateMachin

---
+++ Assets/StateMachine.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StateMachine : MonoBehaviour {
+
+    public static StateMachine Instance;
+
+    enum GameState
+    {
+        Starting,
+        Menu,
+        Menu_About,
+        Menu_Collections,
+        StartGame,
+        Normal,
+        GameOver
+    }
+
+    GameState gameState;
+
+	// Use this for initialization
+	void Start () {
+        if (Instance == null) Instance = this;
+        if (Instance != this) Destroy(this);
+        DontDestroyOnLoad(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    void Starting()
+    {
+        
+    }
+}
